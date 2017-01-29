8ab6f49205460cd6d20578a2db5b27111547fffa
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-01-29 10:09:21 -0500

    main page done

--- Scripts/portfolio.js
+++ Scripts/portfolio.js
@@ -1,8 +1,16 @@
 /* JAVA Script Here*/
 
-var typist;
-		typist = document.querySelector("#typist-element");
-		new Typist(typist, {
-		  letterInterval: 60,
-		  textInterval: 3000
-		});
+//step1- create a reference to an element
+var Biography = document.getElementById("BioPara");
+//step2- create a variable that contain content
+var myContent = "I’m a web developer. I spend my whole day, practically every day, experimenting with HTML, CSS, and JavaScript; I build websites that delight and inform. I do it well. I enjoy work that challenges me to learn something new and stretch in a different direction. I do my best to stay on top of changes in the state of the art so that I can meet challenges with tools well suited to the job at hand. I moved to Alberta from India a year ago, learned the language, and have been studying at Gerogian College in Barrie for the time being.";
+//step3- assign the variable with your content to the refrence variable
+BioPara.textContent = myContent;
+
+var MyName = document.getElementById("MyName");
+var myContent= "RAJVINDER SINGH YOGI";
+MyName.textContent=myContent;
+
+var WebDeveloper = document.getElementById("WebDev");
+var myContent="WEB DEVELOPER | BIOGRAPHY";
+WebDev.textContent=myContent;
